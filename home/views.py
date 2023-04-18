c79153f159e0a7abfb7d9c3f804fc12554346749
Sam-Verma <samverma155@gmail.com>
2023-04-18 13:34:24 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render, HttpResponse, redirect
+from home.models import Contact
+from django.contrib import messages
+from blog.models import Post
+from django.contrib.auth.models import User
+from django.contrib.auth  import authenticate,  login, logout
+# Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+
+def about(request):
+    return render(request,'home/about.html')
+
+def contact(request):
+    
+    
+    if request.method=="POST":
+        
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        content =request.POST.get('content')
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+            messages.error(request,'Please Fill The Form Correctly')
+        else:    
+            contact=Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request,'Form Submitted Successfully')
+    return render(request, "home/contact.html")
+
+def search(request):
+    query=request.GET['query']
+    if len(query)>78:
+        allPosts=Post.objects.none()
+    else:
+        allPostsTitle= Post.objects.filter(title__icontains=query)
+    #    if there is something in the querywhich resembles with author then it will show resulet
+        allPostsAuthor= Post.objects.filter(author__icontains=query)
+        allPostsContent =Post.objects.filter(content__icontains=query)
+        allPosts=  allPostsTitle.union(allPostsContent, allPostsAuthor)
+    if allPosts.count()==0:
+        messages.warning(request, "No search results found. Please refine your query.")
+    params={'allPosts': allPosts, 'query': query}
+    return render(request, 'home/search.html', params)
+
+def handleSignup(request):
+    if request.method == 'POST':
+        # Get the post parameters
+        # username is dictionary
+        username=request.POST['username']
+        email=request.POST['email']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+
+        # check for errorneous input
+        if len(username)<10:
+            messages.error(request, "Your user name is too short")
+            return redirect('home')
+        # username is should be alphanumeric
+        if not username.isalnum():
+            messages.error(request, "User name should only contain letters and numbers")
+            return redirect('home')
+        # chech password match
+        if (pass1 != pass2):
+            messages.error(request, "Passwords do not match")
+            return redirect('home')
+        
+        
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name= fname
+        myuser.last_name= lname
+        myuser.save()
+        messages.success(request, " Your account has been successfully created")
+        return redirect('home')
+        
+    else:
+        return HttpResponse("error 404")
+    
+def handeLogin(request):
+    if request.method=="POST":
+        # Get the post parameters
+        loginusername=request.POST['loginusername']
+        loginpassword=request.POST['loginpassword']
+
+        user=authenticate(username= loginusername, password= loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid credentials! Please try again")
+            return redirect("home")
+
+    return HttpResponse("404- Not found")
+   
+
+    return HttpResponse("login")
+
+def handelLogout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('home')
